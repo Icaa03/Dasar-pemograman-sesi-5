59985bed761dd4fadc4424a8ca7549c843c35654
Anisa Cikal Virgifiani <160223842+Icaa03@users.noreply.github.com>
2024-03-21 23:11:07 +0700

    Add files via upload

---
+++ 1. Aku cinta indonesia.py
@@ -0,0 +1,10 @@
+# 1. 1. Aku Cinta Indonesia 
+#    2. Aku Cinta Indonesia
+#    3. Aku Cinta Indonesia
+
+a = 1
+max = 3
+
+while (a <= max):
+    print (a, end=" Aku Cinta Indonesia \n")
+    a +=1
---
+++ 4. (7.6=42) (7.7=49) (7.8=56) (7.9=63) (7.10=70).py
@@ -0,0 +1,7 @@
+a = 7
+b = 6
+c = 42
+for i in range (5) :
+    print(a, "*", b, "=", c)
+    b += 1
+    c = a*b
